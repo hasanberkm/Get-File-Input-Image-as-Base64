4734d0d4a92e3bebc071cbf81fccfd1fd4c262b6
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2020-05-05 13:04:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-if ($('input[name=file]').prop('files')[0] != undefined) {
-  var reader = new FileReader();
-  reader.readAsDataURL($('input[name=file]').prop('files')[0]);
-  reader.onload = function(e) {
-    $("#selected_image").attr("src", e.target.result);
-  };
-}
+$('#file').change(function() {
+    if ($(this).prop('files')[0] != undefined) {
+      console.log("1");
+      var reader = new FileReader();
+      reader.readAsDataURL($('input[name=file]').prop('files')[0]);
+      reader.onload = function(e) {
+        $(".selected_image").attr("src", e.target.result);
+      };
+    }
+})
