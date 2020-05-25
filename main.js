5c1fd75afad68c7eee53081ff7f5b1d30cb5d7b5
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2020-05-25 11:59:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $('#file').change(function() {
     if ($(this).prop('files')[0] != undefined) {
-      console.log("1");
       var reader = new FileReader();
       reader.readAsDataURL($('input[name=file]').prop('files')[0]);
       reader.onload = function(e) {
